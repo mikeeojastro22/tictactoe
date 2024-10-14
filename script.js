5e9bbfd64227546ee2b02d7feafeffdcdf7424b0
Mikee Ojastro <mikeeojastro@gmail.com>
2024-10-14 18:29:31 +0800

    October 14, 2024

---
+++ script.js
@@ -0,0 +1,23 @@
+let board = document.getElementById("board");
+
+let gameBoard = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+let playerTurn1 = true;
+
+function createBoard(){
+
+}
+
+function addMove(element, boxNumber){
+
+}
+
+function updateBoard(element, boxNumber){
+
+}
+
+createBoard();
